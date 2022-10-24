1599f8e47490af0fb53c1bdb338b6b3909aec2f3
Peter Macgregor <macgregor.pr@gmail.com>
2022-10-24 23:57:25 +0100

    Add size factor

--- flask_app.py
+++ flask_app.py
@@ -15,9 +15,12 @@ def get_mid_clusters():
         #   - start_year (int)
         #   - end_year (int)
         #   - country (string)
+        #   - size (float from 0 to 1)
         start_year = int(request.args.get('start_year'))
         end_year = int(request.args.get('end_year'))
         country = request.args.get('country')
+        size = float(request.args.get('size'))
+        assert 0 <= size <= 1
     except Exception:
         # We could not find the required arguments
         response = flask.make_response("Invalid request", 400)
@@ -25,7 +28,7 @@ def get_mid_clusters():
         return response
 
     try:
-        this_cluster, that_cluster = cluster.find_mid_clusters(start_year, end_year, country)
+        this_cluster, that_cluster = cluster.find_mid_clusters(start_year, end_year, country, size)
     except Exception:
         # We could not find the clusters
         response = flask.make_response("Internal error", 500)
